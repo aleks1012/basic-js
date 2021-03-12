0fe786bca14052dcf2aae275255e4ba36eb313c2
aleks1012 <aleksei.zharskii@gmail.com>
2021-03-12 11:39:18 +0400

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ if (!Array.isArray(members)) {
+ 	return false;
+ }
+ const arr = [];
+ 	for (const item of members) {
+ 		if (typeof(item) === 'string') {
+ 			if (item[0] === ' ') {
+ 				let i = 1;
+ 				while (item[i] === ' ') {
+ 					i+= 1;
+ 				}
+ 				const letter = item[i].toUpperCase();
+ 				arr.push(letter);
+ 			}
+ 			const letter = item[0].toUpperCase();
+ 				arr.push(letter);
+ 		}
+ 	}
+ 	if (arr.length === 0) {
+ 		return false;
+ 	}
+ 	const sortedArr = arr.sort();
+ 	/*if (arr.sort()[0] === ' ') {
+ 		return arr.sort().shift.join('');
+ 	}*/
+ 	return sortedArr.join('').trim();
 };
